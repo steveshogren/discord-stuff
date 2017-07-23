6315b4ef560f4c0541f371c34c905eb98a4e1ed5
jack <steve.a.shogren@gmail.com>
2017-07-23 12:23:29 -0400

    Added a csv parser and spitter-outer

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('https');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -21,10 +22,29 @@ const makeBanTimer = function(message, totalSeconds, remainingSeconds) {
         message.channel.send(remainingSeconds + banMessage);
     }, milliTimeLeft(totalSeconds, remainingSeconds));
 };
+const getGame = function getGame(gameId, callback) {
+    return http.get("https://api.agora.gg/games/" + gameId + "?lc=en&ssl=true", function(response) {
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            var parsed = JSON.parse(body);
+            callback(parsed);
+        });
+    });
+};
 
 client.on('message', message => {
   if (message.content === 'ping') {
     message.reply('pong');
+  }
+  if (message.content.includes('custom.bot.csv')) {
+      var gameid = message.content.substring(14).trim();
+      getGame(gameid, function(d) {
+          const game = parseGame(d);
+          message.reply(game);
+      });
   }
   if (message.content === 'custom timer') {
       message.channel.send('120' + banMessage);
@@ -41,6 +61,104 @@ client.on('message', message => {
 
 
 });
+
+
+const addPlayer = function(p, teamDamage) {
+    return "\"" + p.name + "\""
+        + "," + p.elo
+        + "," + p.hero;
+    // + "," + p.level
+    // + "," + p.kills
+    // + "," + p.deaths
+    // + "," + p.assists
+    // + "," + p.towers
+    // + "," + p.heroDamage
+    // + "," + p.minionDamage
+    // + "," + p.jungleDamage
+    // + "," + p.towerDamage
+    // + "," + p.heroGamesPlayed
+    // + "," + p.heroWins
+    // + "," + p.heroKills
+    // + "," + p.heroDeaths
+    // + "," + p.heroAssists
+    // + "," + p.totalGamesPlayed
+    // + "," + p.totalWins
+    // + "," + p.totalKills
+    // + "," + p.totalDeaths
+    // + "," + p.totalAssists
+    //    + "," + p.totalTowers;
+};
+
+const addPlayerDamage = function(p, teamDamage) {
+    teamDamage.heroDamage   = teamDamage.heroDamage    ? teamDamage.heroDamage : 0;
+    teamDamage.minionDamage = teamDamage.minionDamage  ? teamDamage.minionDamage : 0;
+    teamDamage.jungleDamage = teamDamage.jungleDamage  ? teamDamage.jungleDamage : 0;
+    teamDamage.towerDamage  = teamDamage.towerDamage   ? teamDamage.towerDamage : 0;
+    teamDamage.heroDamage += p.heroDamage;
+    teamDamage.minionDamage += p.minionDamage;
+    teamDamage.jungleDamage+= p.jungleDamage;
+    teamDamage.towerDamage+= p.towerDamage;
+    return teamDamage;
+};
+
+const parseGame = function(d) {
+    const t0Damage =
+              addPlayerDamage(d.data.teams[0][4],
+              addPlayerDamage(d.data.teams[0][3],
+              addPlayerDamage(d.data.teams[0][2],
+              addPlayerDamage(d.data.teams[0][1],
+                              addPlayerDamage(d.data.teams[0][0], {})))));
+
+    const t1Damage =
+              addPlayerDamage(d.data.teams[1][4],
+                              addPlayerDamage(d.data.teams[1][3],
+                                              addPlayerDamage(d.data.teams[1][2],
+                                                              addPlayerDamage(d.data.teams[1][1],
+                                                                              addPlayerDamage(d.data.teams[1][0], {})))));
+
+
+    const t0Elo = (d.data.teams[0][4].elo
+                   + d.data.teams[0][3].elo
+                   + d.data.teams[0][2].elo
+                   + d.data.teams[0][1].elo
+                   + d.data.teams[0][0].elo
+                  )/5;
+    const t1Elo = (d.data.teams[1][4].elo
+                   + d.data.teams[1][3].elo
+                   + d.data.teams[1][2].elo
+                   + d.data.teams[1][1].elo
+                   + d.data.teams[1][0].elo
+                  )/5;
+    const game =
+              (Math.floor(d.data.length/60) + " minutes")
+              + "," + (d.data.winningTeam == 0 ? "Team One Won" : "Team Two Won")
+              + "," + t0Elo
+              + "," + t0Damage.heroDamage
+              + "," + t0Damage.minionDamage
+              + "," + t0Damage.jungleDamage
+              + "," + t0Damage.towerDamage
+              + addPlayer(d.data.teams[0][0])
+              + addPlayer(d.data.teams[0][1])
+              + addPlayer(d.data.teams[0][2])
+              + addPlayer(d.data.teams[0][3])
+              + addPlayer(d.data.teams[0][4])
+              + "\"," + t1Elo
+              + "," + t1Damage.heroDamage
+              + "," + t1Damage.minionDamage
+              + "," + t1Damage.jungleDamage
+              + "," + t1Damage.towerDamage
+              + addPlayer(d.data.teams[1][0])
+              + addPlayer(d.data.teams[1][1])
+              + addPlayer(d.data.teams[1][2])
+              + addPlayer(d.data.teams[1][3])
+              + addPlayer(d.data.teams[1][4]);
+    console.log(game);
+    return game;
+};
+
+//getGame("9d7403ba86d44193b6773847bb6e1bb9", function(d) {
+//    parseGame(d);
+//});
 
 
 client.login(token);
