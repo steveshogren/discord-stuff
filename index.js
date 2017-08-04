776f5c2f6906db4a1376fef8dab9d330fc129339
jack <steve.a.shogren@gmail.com>
2017-08-04 06:59:41 -0400

    Refactoring the js and testing it

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const token = config.get('token');
 const banMessage = " seconds left";
 
 client.on('ready', () => {
-  console.log('I am ready!');
+    console.log('I am ready!');
 });
 
 const milliTimeLeft = function(totalSeconds, remainingSeconds) {
@@ -36,29 +36,29 @@ const getGame = function getGame(gameId, callback) {
 };
 
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
-  if (message.content.includes('custom.bot.csv')) {
-      var gameid = message.content.substring(14).trim();
-      getGame(gameid, function(d) {
-          //console.log(d);
-          const game = parseGame(d);
-          message.reply(game);
-      });
-  }
-  if (message.content === 'custom timer') {
-      message.channel.send('120' + banMessage);
-      makeBanTimer(message, 120, 90);
-      makeBanTimer(message, 120, 60);
-      makeBanTimer(message, 120, 30);
-      makeBanTimer(message, 120, 15);
-      makeBanTimer(message, 120, 10);
-      makeBanTimer(message, 120, 5);
-      setTimeout(function() {
-          message.channel.send('WHERES YOUR BAN?');
-      }, 1000 * 120);
-  }
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+    if (message.content.includes('custom.bot.csv')) {
+        var gameid = message.content.substring(14).trim();
+        getGame(gameid, function(d) {
+            //console.log(d);
+            const game = parseGame(d);
+            message.reply(game);
+        });
+    }
+    if (message.content === 'custom timer') {
+        message.channel.send('120' + banMessage);
+        makeBanTimer(message, 120, 90);
+        makeBanTimer(message, 120, 60);
+        makeBanTimer(message, 120, 30);
+        makeBanTimer(message, 120, 15);
+        makeBanTimer(message, 120, 10);
+        makeBanTimer(message, 120, 5);
+        setTimeout(function() {
+            message.channel.send('WHERES YOUR BAN?');
+        }, 1000 * 120);
+    }
 
 
 });
@@ -68,47 +68,60 @@ const addPlayer = function(p, teamDamage) {
     return "," + p.name
         + "," + p.elo
         + "," + p.hero;
-    // + "," + p.level
-    // + "," + p.kills
-    // + "," + p.deaths
-    // + "," + p.assists
-    // + "," + p.towers
-    // + "," + p.heroDamage
-    // + "," + p.minionDamage
-    // + "," + p.jungleDamage
-    // + "," + p.towerDamage
-    // + "," + p.heroGamesPlayed
-    // + "," + p.heroWins
-    // + "," + p.heroKills
-    // + "," + p.heroDeaths
-    // + "," + p.heroAssists
-    // + "," + p.totalGamesPlayed
-    // + "," + p.totalWins
-    // + "," + p.totalKills
-    // + "," + p.totalDeaths
-    // + "," + p.totalAssists
-    //    + "," + p.totalTowers;
+    // + "," + p.level + "," + p.kills + "," + p.deaths + "," + p.assists + "," + p.towers + "," + p.heroDamage + "," + p.minionDamage + "," + p.jungleDamage + "," + p.towerDamage + "," + p.heroGamesPlayed + "," + p.heroWins + "," + p.heroKills + "," + p.heroDeaths + "," + p.heroAssists + "," + p.totalGamesPlayed + "," + p.totalWins + "," + p.totalKills + "," + p.totalDeaths + "," + p.totalAssists + "," + p.totalTowers;
 };
 
 const addPlayerDamage = function(p, teamDamage) {
-    teamDamage.heroDamage   = teamDamage.heroDamage    ? teamDamage.heroDamage : 0;
-    teamDamage.minionDamage = teamDamage.minionDamage  ? teamDamage.minionDamage : 0;
-    teamDamage.jungleDamage = teamDamage.jungleDamage  ? teamDamage.jungleDamage : 0;
-    teamDamage.towerDamage  = teamDamage.towerDamage   ? teamDamage.towerDamage : 0;
     teamDamage.heroDamage += p.heroDamage;
     teamDamage.minionDamage += p.minionDamage;
-    teamDamage.jungleDamage+= p.jungleDamage;
-    teamDamage.towerDamage+= p.towerDamage;
+    teamDamage.jungleDamage += p.jungleDamage;
+    teamDamage.towerDamage += p.towerDamage;
     return teamDamage;
 };
 
+const comp = function(f, g) {
+    return function(x) {
+        return g(f(x));
+    };
+};
+
+const map = function(f, coll) {
+    var ret = [];
+    for(var i = 0; i < coll.length; i++){
+        ret = ret.concat([f(coll[i])]);
+    }
+    return ret;
+};
+const reduce = function(f, seed, coll) {
+    var next = seed;
+    for(var i = 0; i < coll.length; i++){
+        next = f(coll[i], next);
+    }
+    return next;
+};
+
+const getTeamDamage = function(team) {
+    var damage = {
+        heroDamage: 0,
+        minionDamage: 0,
+        jungleDamage: 0,
+        towerDamage: 0
+    };
+    return reduce(function(p, next) {
+        return addPlayerDamage(team[p])(next);
+    }, damage, [0,1,2,3,4]);
+};
+
 const parseGame = function(d) {
+    const t0Dam = getTeamDamage(d.teams[0]);
+    const t1Dam = getTeamDamage(d.teams[1]);
+
     const t0Damage =
               addPlayerDamage(d.teams[0][4],
-              addPlayerDamage(d.teams[0][3],
-              addPlayerDamage(d.teams[0][2],
-              addPlayerDamage(d.teams[0][1],
-                              addPlayerDamage(d.teams[0][0], {})))));
+                              addPlayerDamage(d.teams[0][3],
+                                              addPlayerDamage(d.teams[0][2],
+                                                              addPlayerDamage(d.teams[0][1],
+                                                                              addPlayerDamage(d.teams[0][0], {})))));
 
     const t1Damage =
               addPlayerDamage(d.teams[1][4],
@@ -158,9 +171,47 @@ const parseGame = function(d) {
     return game;
 };
 
+
+const runTests = function() {
+    var results = "";
+    const assertEqual = function(x, y) {
+        if(x === y) {
+            results +=".";
+        } else {
+            results += ("\nFailed. " + x + " does not equal " + y + "\n");
+        }
+    };
+
+    assertEqual(6,reduce(function(x,n) {return x + n;}, 0, [0,1,2,3]));
+    assertEqual(0,reduce(function(x,n) {return x + n;}, 0, []));
+    assertEqual(0,reduce(function(x,n) {return x + n;}, 0, [0]));
+
+    assertEqual([1,2,3].toString(),map(function(x) {return x + 1;}, [0,1,2]).toString());
+    assertEqual([].toString(),map(function(x) {return x + 1;}, []).toString());
+
+    const players = [
+        { heroDamage:1, minionDamage:1, jungleDamage:1, towerDamage:1 },
+        { heroDamage:0, minionDamage:0, jungleDamage:0, towerDamage:0 },
+        { heroDamage:3, minionDamage:3, jungleDamage:3, towerDamage:3 },
+        { heroDamage:2, minionDamage:2, jungleDamage:2, towerDamage:2 },
+        { heroDamage:4, minionDamage:4, jungleDamage:4, towerDamage:4 }
+
+    ];
+    const expectedSum = {
+        heroDamage:10,
+        minionDamage:10,
+        jungleDamage:10,
+        towerDamage:10
+    };
+
+    assertEqual(JSON.stringify(expectedSum, null, '\t'), JSON.stringify(getTeamDamage(players),null,'\t'));
+
+    console.log(results);
+};
+
 //getGame("9d7403ba86d44193b6773847bb6e1bb9", function(d) {
 //    parseGame(d);
 //});
 
-
-client.login(token);
+runTests();
+//client.login(token);
